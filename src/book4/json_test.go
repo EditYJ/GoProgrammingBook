f49e3d90f76b6536ff9eb09730b4e756bd23032f
EditYJ <158392613@qq.com>
2019-07-27 17:20:32 +0800

    Update json_test.go

--- src/book4/json_test.go
+++ src/book4/json_test.go
@@ -5,7 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
+	"strings"
 	"testing"
+	"time"
 )
 
 type Movie struct {
@@ -28,13 +32,16 @@ var movies = []Movie{
 	},
 }
 
+var titles []struct{Title string}
+
 func TestJsonType(t *testing.T) {
-	//data,err := json.Marshal(movies)	// 紧凑输出
-	data,err := json.MarshalIndent(movies, "", "	")	// 格式化输出
+	data,err := json.Marshal(movies)	// 紧凑输出
+	//data,err := json.MarshalIndent(movies, "", "	")	// 格式化输出
+	err = json.Unmarshal(data, &titles)	// json转对象
 	if err !=nil{
 		log.Fatalf("JSON marshaling failed: %s", err)
 	}
-	fmt.Printf("%s\n",data)
+	fmt.Printf("%s\n",titles)
 }
 
 //////////////////// test github interface
@@ -49,4 +56,48 @@ type IssuesSearchResult struct {
 type Issue struct {
 	Number int
 	HTMLURL string `json:"html_url"`
+	Title string
+	State string
+	User *User
+	CreateAt time.Time `json:"created_at"`
+	Body string
+}
+
+type User struct {
+	Login string
+	HTMLURL string `json:"html_url"`
+}
+
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	fmt.Println(q)
+	resp, err := http.Get(IssuesUrl + "?q=" +q)
+	if err !=nil{
+		return nil,err
+	}
+
+	if resp.StatusCode != http.StatusOK{
+		resp.Body.Close()
+		return nil, fmt.Errorf("搜索失败: %s",resp.Status)
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result);err != nil{
+		resp.Body.Close()
+		return nil,err
+	}
+
+	resp.Body.Close()
+	return &result, nil
+}
+
+func TestGitHubInterface(t *testing.T) {
+	result,err := SearchIssues([]string{"golang","flutter"})
+	if err!=nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues: \n", result.TotalCount)
+	for _,item := range result.Items{
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User, item.Title)
+	}
 }
